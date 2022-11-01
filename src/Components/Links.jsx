1a1c4c6a70cc4deeb0c5c5fbb6bbdbd35b6a14ce
salamah <salamahjimoh@gmail.com>
2022-11-01 09:43:35 -0700

    remove p tags

--- src/Components/Links.jsx
+++ src/Components/Links.jsx
@@ -7,20 +7,16 @@ const Links = () => {
         <a className="btn" href="https://twitter.com/salamah_jimoh">Twitter Link</a>
         <a className="btn" href="https://training.zuri.team/"  id='btn__zuri'>Zuri Team</a>
         <a className="btn" id='books'
-        href="http://books.zuri.team">Books
-        <p>Read from our library of exclusive books</p>
+        href="http://books.zuri.team" title='Read from our library of exclusive books'>Books
       </a>
       <a className="btn" id='book__python'
-        href="https://books.zuri.team/python-for-beginners?ref_id=<Salamah>,"  >Python
-        <p>Get unlimited access to Python books</p>
+        href="https://books.zuri.team/python-for-beginners?ref_id=<Salamah>," title='Get unlimited access to Python books'  >Python
       </a>
       <a className="btn"
-        href="https://background.zuri.team"  id='pitch'>Guidance?
-        <p>We run a background check for codes by offering reviews, mentorships and guidance</p>
+        href="https://background.zuri.team"  id='pitch' title='We run a background check for codes by offering reviews, mentorships and guidance'>Guidance?
       </a>
       <a className="btn"
-        href="https://books.zuri.team/design-rules"  id='book__design'>Free Book?
-        <p>Be among the lucky people that take home this amazing book for free!</p>
+        href="https://books.zuri.team/design-rules"  id='book__design' title='Be among the lucky people that take home this amazing book for free!'>Free Book?
       </a>
       <div className="socials">
         <a href=""><img src={Slack} alt="Slack icon" /></a>
